2baa3473b0aeb99eeb04c032bae59407e43475d7
deflinhec <falconlegend810518@gmail.com>
2021-02-03 18:30:39 +0800

    Update build_docker_image.ps1

--- scripts/build_docker_image.ps1
+++ scripts/build_docker_image.ps1
@@ -1,12 +1,14 @@
-# 1. build docker images
+# 1. build images
 docker build --rm -t gsx2jsonpp ..
+
+# 2. cleanup container
 docker stop gsx2jsonpp
 docker rm gsx2jsonpp
 
-# 2. launch container
+# 3. launch container
 docker run -it -d -p 5000:8080 `
 -v ${PWD}\..\bin\volume:/workspace `
 --name gsx2jsonpp gsx2jsonpp
 
-# 3. bash entry
+# 4. bash entry
 docker exec -it gsx2jsonpp bash
